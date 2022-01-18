0d0bca3ed11c69dcabd677f6504fdb99a63613b9
Ivan Dimitrov <biobeehive@abv.bg>
2022-01-18 10:54:07 +0200

    Initial Commit

---
+++ ESP-relay/ESP_relayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESP_relayApp.swift
+//  ESP-relay
+//
+//  Created by Dosi Dimitrov on 18.01.22.
+//
+
+import SwiftUI
+
+@main
+struct ESP_relayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
